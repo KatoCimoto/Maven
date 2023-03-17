e63eba5ee602bb5bdc6b0e33282dac7f204f0c61
Shabrin Anton <dogue.13@yahoo.com>
2023-03-17 16:54:57 +0200

    test

---
+++ src/main/java/mathLib/MathLibrary.java
@@ -0,0 +1,32 @@
+package mathLib;
+
+  public class MathLibrary {
+
+    public static double add(double a, double b) {
+      return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+      return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+      return a * b;
+    }
+
+    public static double divide(double a, double b) {
+      return a / b;
+    }
+
+    public static double power(double a, double b) {
+      return Math.pow(a, b);
+    }
+
+    public static double sqrt(double a) {
+      return Math.sqrt(a);
+    }
+
+    public static double abs(double a) {
+      return Math.abs(a);
+    }
+  }
